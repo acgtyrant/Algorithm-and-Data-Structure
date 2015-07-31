f8e76ed179450e05b36e8423d706c9eaa9729109
acgtyrant <acgtyrant@gmail.com>
2015-07-31 20:45:15 +0800

    merge-sort: uncomplement done
    run failed, need to fix some bugs

---
+++ merge-sort.h
@@ -0,0 +1,10 @@
+#ifndef MERGE_SORT_H_
+#define MERGE_SORT_H_
+
+#include <vector>
+
+void MergeSort(
+    const std::vector<int>::iterator first,
+    const std::vector<int>::iterator last);
+
+#endif  // MERGE_SORT_H_
