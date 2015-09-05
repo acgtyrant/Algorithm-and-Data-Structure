f36c5fc8b8b0d2c8684cca51c6c5ac4a43323865
acgtyrant <acgtyrant@gmail.com>
2015-09-05 19:12:30 +0800

    selection-sort: use template to generification

--- selection-sort.h
+++ selection-sort.h
@@ -1,10 +1,14 @@
 #ifndef SELECTION_SORT_H_
 #define SELECTION_SORT_H_
 
-#include <vector>
+#include <algorithm>
 
+template <typename ForwardIterator>
 void SelectionSort(
-    std::vector<int>::iterator first,
-    std::vector<int>::iterator last);
+    ForwardIterator first,
+    ForwardIterator last) {
+  for (auto iterator = first; iterator != last; ++iterator)
+    std::iter_swap(iterator, std::min_element(iterator, last));
+}
 
 #endif // SELECTION_SORT_H_
