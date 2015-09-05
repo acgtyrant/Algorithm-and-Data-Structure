77fae8c7215d76bd538de0917cf85e85f094b62e
acgtyrant <acgtyrant@gmail.com>
2015-09-05 19:05:26 +0800

    binary-search: use template to generification

--- binary-search.h
+++ binary-search.h
@@ -6,17 +6,36 @@
 // Checks if an element equivalent to value appears within the range
 // [first, last). There is std::binary_search exists, so do not use namespace
 // std.
+
+template <typename ForwardIterator, typename T>
 bool BinarySearch(
-    const std::vector<int>::const_iterator first,
-    const std::vector<int>::const_iterator last,
-    const int value);
+    const ForwardIterator first,
+    const ForwardIterator last,
+    const T value) {
+  if (first == last) return false;
+  auto middle = first + (last - first) / 2;
+  if (*middle == value) return true;
+  if (*middle > value) return BinarySearch(first, middle, value);
+  if (*middle < value) return BinarySearch(middle + 1, last, value);
+}
 
 namespace iteration {
 
+template <typename ForwardIterator, typename T>
+// first and last are not const.
 bool BinarySearch(
-    std::vector<int>::const_iterator first,
-    std::vector<int>::const_iterator last,
-    const int value);
+    ForwardIterator first,
+    ForwardIterator last,
+    const T value) {
+  ForwardIterator middle;
+  while (first != last) {
+    middle = first + (last - first) / 2;
+    if (*middle == value) return true;
+    if (*middle > value) last = middle;
+    if (*middle < value) first = middle + 1;
+  }
+  return false;
+}
 
 }  // namespace iteration
 
