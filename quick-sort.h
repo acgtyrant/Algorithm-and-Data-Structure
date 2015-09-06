600e65bfc34326a73abea48b1bab912404f35016
acgtyrant <acgtyrant@gmail.com>
2015-09-06 16:13:17 +0800

    quick-sort: use template to generification

--- quick-sort.h
+++ quick-sort.h
@@ -1,10 +1,56 @@
 #ifndef QUICK_SORT_H_
 #define QUICK_SORT_H_
 
-#include <vector>
+#include <algorithm>
 
+// bad namespace scope while it is included in headfile
+namespace {
+
+template <typename ForwardIterator>
+ForwardIterator MedianOf3(
+    ForwardIterator one,
+    ForwardIterator two,
+    ForwardIterator three) {
+  return std::min_element(one, std::min_element(two, three));
+}
+
+constexpr int cutoff = 3;
+
+}  // namespace
+
+template <typename RandomIterator>
 void QuickSort(
-    const std::vector<int>::iterator first,
-    const std::vector<int>::iterator last);
+    RandomIterator first,
+    RandomIterator last) {
+  if (last - first < cutoff) return std::sort(first, last);
+  std::random_shuffle(first + 1, last);
+  auto low = first + 1;
+  auto high = last - 1;
+  auto partition = first;
+  if (first + 2 < last) {
+    auto median = MedianOf3(
+        first,
+        first + (last - first) / 2,
+        last - 1);
+    std::iter_swap(partition, median);
+  }
+  while (low <= high) {
+    while (*low < *partition && low != last) ++low;
+    while (*high > *partition && high != first) --high;
+    if (low < high) std::iter_swap(low, high);
+  }
+  std::iter_swap(first, low - 1);
+  QuickSort(first, low - 1);
+  QuickSort(low, last);
+}
+
+namespace anti_duplicate {
+
+template <typename RandomIterator>
+void QuickSort(
+    RandomIterator first,
+    RandomIterator last);
+
+}  // anti_duplicate
 
 #endif // QUICK_SORT_H_
